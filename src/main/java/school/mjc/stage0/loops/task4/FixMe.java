05e0687d18782a2c6a56805fa840d7497f68295d
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-11 00:27:40 +0200

    Finished.

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -3,12 +3,12 @@ package school.mjc.stage0.loops.task4;
 public class FixMe {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            if (i % 2 == 0) {
+            if (i % 2 == 0)
                 System.out.println(i);
-            } else if (i == 7) {
+            else if (i == 7) {
                 System.out.println("last");
-                break;
                 System.out.println("will I be printed?");
+                break;
             }
         }
     }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if(numberToSkip > lastInRow)
+            System.out.println("number to skip is bigger then the last");
+        else if(lastInRow < 0)
+            System.out.println("last number in row is negative");
+        else {
+            int sum =0;
+            int sumSipped =0;
+            for(int i=0; i<=lastInRow; i++){
+                if(i <= numberToSkip) {
+                    sumSipped += i;
+                }
+                else{
+                    sum += i;
+                }
+            }
+            System.out.println("skipped sum is " + sumSipped);
+            System.out.println("counted sum is " + sum);
+        }
 
     }
 }
